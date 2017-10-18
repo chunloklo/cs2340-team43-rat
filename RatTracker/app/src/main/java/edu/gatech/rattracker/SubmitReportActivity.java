c113eebf30b9917f91594e995243cadb0822b23f
chunloklo <clo42@gatech.edu>
2017-10-18 18:01:06 -0400

    some initial changes to increase usability/reliability

--- RatTracker/app/src/main/java/edu/gatech/rattracker/SubmitReportActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/rattracker/SubmitReportActivity.java
@@ -6,14 +6,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 
@@ -32,8 +29,12 @@ public class SubmitReportActivity extends AppCompatActivity {
 
         // fetch text inputs, coordinates, and buttons
         final EditText locationType = (EditText) findViewById(R.id.typeInput);
-        final EditText address1 = (EditText) findViewById(R.id.address1Input);
-        final EditText address2 = (EditText) findViewById(R.id.address2Input);
+        final EditText addressText = (EditText) findViewById(R.id.addressInput);
+        final EditText cityText = (EditText) findViewById(R.id.cityInput);
+        final Spinner boroughText = (Spinner) findViewById(R.id.spinner);
+        boroughText.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Borough.values()));
+
+        final EditText zipText = (EditText) findViewById(R.id.zipInput);
         final EditText longitudeText = (EditText) findViewById(R.id.longitudeInput);
         final EditText latitudeText = (EditText) findViewById(R.id.latitudeInput);
 
@@ -55,38 +56,36 @@ public class SubmitReportActivity extends AppCompatActivity {
                 Calendar c = Calendar.getInstance();
                 final long date = c.getTimeInMillis();
                 final String type = Uri.encode(locationType.getText().toString().trim());
-                final String address = address1.getText().toString().trim();
-                String address2Text = address2.getText().toString().trim();
-                final String borough = address2Text.split(",")[0].trim();
-                final String city = "New York City"; // TODO: add logic for city that is not NYC
-                final short zip = Short.parseShort(address2Text.substring(address2Text.length() - 5));
-                final double longitude = Double.parseDouble(longitudeText.getText().toString()); // TODO: negative coordinates
-                final double latitude = Double.parseDouble(latitudeText.getText().toString());
-                final Sighting sighting = new Sighting("", date, type, zip, address, city, borough, longitude, latitude);
+                final String address = addressText.getText().toString().trim();
+                final String borough = boroughText.getSelectedItem().toString();
+                final String city = cityText.getText().toString();
+
+                Long zip;
+                double longitude;
+                double latitude;
+                try {
+                    zip = Long.parseLong(zipText.getText().toString());
+                    longitude = Double.parseDouble(longitudeText.getText().toString()); // TODO: negative coordinates
+                    latitude = Double.parseDouble(latitudeText.getText().toString());
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error. Please enter valid numbers", Toast.LENGTH_SHORT).show();
+                    Log.e(logTag, "Input conversion failed: " + e.getMessage());
+                    return;
+                }
+
+                final Sighting sighting = new Sighting("NO KEY", date, type, zip, address, city, borough, longitude, latitude);
 
                 // Grabs FirebaseManager
                 final FirebaseManager firebaseManager = FirebaseManager.getInstance();
-                DatabaseReference reportListener = firebaseManager.reportListener();
-                Log.d(logTag, "Report submission by verified user with ID "); // TODO: improve log text
-
-                reportListener.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        Log.d(logTag, "obtaining dataSnapshot");
-                        boolean success = firebaseManager.addSighting(sighting);
-                        if (success) {
-                            Intent report = new Intent(getApplicationContext(), ReportActivity.class);
-                            startActivity(report);
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Failed to submit report", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        Toast.makeText(getApplicationContext(), "Submission failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                boolean success = firebaseManager.addSighting(sighting);
+                if (success) {
+                    Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+                    Intent report = new Intent(getApplicationContext(), ReportActivity.class);
+                    startActivity(report);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to submit report", Toast.LENGTH_SHORT).show();
+                }
 
 
             }
