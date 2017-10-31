02454c56ee1f9873636228d19b5866bd8103cb2b
Matthew Wang <mwang7313@gmail.com>
2017-10-31 16:01:19 -0400

    Changed into a Fragment

--- RatTracker/app/src/main/java/edu/gatech/rattracker/GraphActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/rattracker/GraphActivity.java
@@ -1,23 +1,42 @@
 package edu.gatech.rattracker;
 
-import android.graphics.Canvas;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
 
-import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-import static android.app.PendingIntent.getActivity;
+/**
+ * @author Matthew Wang
+ * @version 1.0
+ */
+public class GraphActivity extends Fragment {
+    private Report report;
+    private DatePicker startDate, endDate;
 
-public class GraphActivity extends AppCompatActivity {
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,5 +80,76 @@ public class GraphActivity extends AppCompatActivity {
         // as we use dates as labels, the human rounding to nice readable numbers
         // is not necessary
         graph.getGridLabelRenderer().setHumanRounding(false);
+    } */
+
+
+    /*
+    public static GraphActivity newInstance() {
+
+        Bundle args = new Bundle();
+
+        GraphActivity fragment = new GraphActivity();
+        fragment.setArguments(args);
+        return fragment;
+    } */
+
+
+//    private GoogleApiClient mGoogleApiClient;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_graph, container, false);
+        
+        // generate Dates
+        Calendar calendar = Calendar.getInstance();
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d2 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d3 = calendar.getTime();
+
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+
+        graph.setTitle("Rat Sightings Over Time");
+        gridLabel.setVerticalAxisTitle("Number of Sightings");
+        gridLabel.setHorizontalAxisTitle("Date");
+
+        // you can directly pass Date objects to DataPoint-Constructor
+        // this will convert the Date to double via Date#getTime()
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3)
+        });
+
+        graph.addSeries(series);
+
+        // set date label formatter
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(d1.getTime());
+        graph.getViewport().setMaxX(d3.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        return rootView;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    public void onStop() {
+        super.onStop();
+    }
+
 }
