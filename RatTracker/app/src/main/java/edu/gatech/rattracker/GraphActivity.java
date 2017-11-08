dee7897e5c62e006564e1415214f1d6a4204d73d
Matthew Wang <mwang7313@gmail.com>
2017-11-07 23:53:38 -0500

    Refactored code, redid how dates were handled to avoid bugs, added helper methods, removed magic numbers, added comments and javadocs

--- RatTracker/app/src/main/java/edu/gatech/rattracker/GraphActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/rattracker/GraphActivity.java
@@ -1,11 +1,8 @@
 package edu.gatech.rattracker;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,10 +21,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -47,80 +41,16 @@ public class GraphActivity extends Fragment {
     private ArrayAdapter<CharSequence> adapter;
     private GraphView graph;
 
-    private class YearMonthFormatter extends DefaultLabelFormatter {
-        @Override
-        public String formatLabel(double value, boolean isValueX) {
-            String month;
-            String year;
-            if (isValueX) {
-                if ((int)value % 100 > 11) {
-                    if ((int)value % 100 < 56) {
-                        value += 88;
-                    }
-                }
-                switch ((int)value % 100) {
-                    case 0:
-                        month = "Jan";
-                        break;
-                    case 1:
-                        month = "Feb";
-                        break;
-                    case 2:
-                        month = "Mar";
-                        break;
-                    case 3:
-                        month = "Apr";
-                        break;
-                    case 4:
-                        month = "May";
-                        break;
-                    case 5:
-                        month = "Jun";
-                        break;
-                    case 6:
-                        month = "Jul";
-                        break;
-                    case 7:
-                        month = "Aug";
-                        break;
-                    case 8:
-                        month = "Sep";
-                        break;
-                    case 9:
-                        month = "Oct";
-                        break;
-                    case 10:
-                        month = "Nov";
-                        break;
-                    case 11:
-                        month = "Dec";
-                        break;
-                    default:
-                        month = "N/A";
-                }
-                year = Integer.toString((int)value / 100);
-                return month + " " + year;
-            } else {
-                return super.formatLabel(value, isValueX);
-            }
-        }
-    }
-
-    private class YearFormatter extends DefaultLabelFormatter {
-        @Override
-        public String formatLabel(double value, boolean isValueX) {
-            String year;
-            if (isValueX) {
-                return Integer.toString((int)value);
-            } else {
-                return super.formatLabel(value, isValueX);
-            }
-        }
-    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_graph, container, false);
+
+        graph = (GraphView) rootView.findViewById(R.id.graph);
+        graph.setTitle("Rat Sightings Over Time");
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+        gridLabel.setVerticalAxisTitle("Number of Sightings");
+        gridLabel.setHorizontalAxisTitle("Date");
 
         spinner = (Spinner) rootView.findViewById(R.id.month_or_year_spinner);
         adapter = ArrayAdapter.createFromResource(getContext(), R.array.month_or_year, android.R.layout.simple_spinner_item);
@@ -134,23 +64,21 @@ public class GraphActivity extends Fragment {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                Log.e("graph", "Nothing selected bug on spinner.")
             }
         });
 
         startDate = (DatePicker) rootView.findViewById(R.id.startDate);
         endDate = (DatePicker) rootView.findViewById(R.id.endDate);
 
-
         Calendar now = Calendar.getInstance();
         int year = now.get(Calendar.YEAR);
         int month = now.get(Calendar.MONTH);
         int day = now.get(Calendar.DAY_OF_MONTH);
 
-
-        startDate.updateDate(year - 1, month, day);
+        //Sets default end date to the current day and the default start date to a month before.
+        startDate.updateDate(year, month - 1, day);
         endDate.updateDate(year, month, day);
-
 
         startDate.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
             @Override
@@ -165,19 +93,10 @@ public class GraphActivity extends Fragment {
                 onGraphReady((String) spinner.getSelectedItem());
             }
         });
-
-        graph = (GraphView) rootView.findViewById(R.id.graph);
-        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
-
-        graph.setTitle("Rat Sightings Over Time");
-        gridLabel.setVerticalAxisTitle("Number of Sightings");
-        gridLabel.setHorizontalAxisTitle("Date");
         return rootView;
     }
 
     public void onGraphReady(final String monthOrYear) {
-        Toast.makeText(getContext(), monthOrYear, Toast.LENGTH_SHORT).show();
-
         FirebaseManager firebaseManager = FirebaseManager.getInstance();
         DatabaseReference reportListener = firebaseManager.reportListener();
 
@@ -190,40 +109,38 @@ public class GraphActivity extends Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 graph.removeAllSeries();
                 ArrayList<Sighting> sightings = new ArrayList<Sighting>();
-                //ArrayList<Date> dates = new ArrayList<Date>();
-                //ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
                 Calendar cal = Calendar.getInstance();
                 Date date;
-                int day;
                 int month;
                 int year;
-                int yearMonth;
+                int unixMonth;
                 HashMap<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
-                HashMap<Long, Integer> frequencies2 = new HashMap<Long, Integer>();
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-
                     try {
                         Sighting sighting = postSnapshot.getValue(Sighting.class);
                         sightings.add(sighting);
                     } catch (Exception e) {
+                        Log.e("graph", "Bad sighting");
                     }
                 }
 
                 for (Sighting sighting : sightings) {
                     date = new Date(sighting.getDate());
-
                     cal.setTime(date);
-
-                    day = cal.get(Calendar.DAY_OF_MONTH);
                     month = cal.get(Calendar.MONTH);
                     year = cal.get(Calendar.YEAR);
 
                     if (monthOrYear.equals("Month")) {
-                        yearMonth = year * 100 + month;
-                        if (frequencies.containsKey(yearMonth)) {
-                            frequencies.put(yearMonth, frequencies.get(yearMonth) + 1);
+                        /* creates an int whose first 4 digits are the year and the last 2 are the month (0-11)
+                         of the sighting used to calculate the "Unix month" in the calculateUnixMonth() helper method.
+                         */
+                        unixMonth = calculateUnixTimeInMonths(month, year);
+
+                        //Creates hashmap mapping x values (dates) in graph to y values (frequencies).
+                        if (frequencies.containsKey(unixMonth)) {
+                            frequencies.put(unixMonth, frequencies.get(unixMonth) + 1);
                         } else {
-                            frequencies.put(yearMonth, 1);
+                            frequencies.put(unixMonth, 1);
                         }
                     } else {
                         if (frequencies.containsKey(year)) {
@@ -234,39 +151,37 @@ public class GraphActivity extends Fragment {
                     }
                 }
 
-                ArrayList<Integer> yearMonths = new ArrayList<Integer>(frequencies.keySet());
-                Collections.sort(yearMonths);
-                if (yearMonths.size() == 1) {
-                    yearMonths.add(yearMonths.get(0) + 1);
-                    frequencies.put(yearMonths.get(1), 0);
+                ArrayList<Integer> dates = new ArrayList<Integer>(frequencies.keySet());
+                Collections.sort(dates);
+                //Creates an zeroed data point if there's only 1 data point because GraphView can't show a single data point.
+                if (dates.size() == 1) {
+                    dates.add(dates.get(0) + 1);
+                    frequencies.put(dates.get(1), 0);
                 }
                 DataPoint[] dataPoints = new DataPoint[frequencies.size()];
                 for (int i = 0; i < dataPoints.length; i++) {
-                    dataPoints[i] = new DataPoint(yearMonths.get(i), frequencies.get(yearMonths.get(i)));
+                    dataPoints[i] = new DataPoint(dates.get(i), frequencies.get(dates.get(i)));
                 }
-                //For debugging
-                for (int i = 0; i < yearMonths.size(); i++) {
-                    Log.e("graph", Long.toString(yearMonths.get(i)));
-                    Log.e("graph", Long.toString(frequencies.get(yearMonths.get(i))));
-                }
-                if (!yearMonths.isEmpty()) {
+                if (!dates.isEmpty()) {
                     BarGraphSeries<DataPoint> series = new BarGraphSeries<>(dataPoints);
+                    //Selects formatter for graph depending on if sightings are sorted by month or year.
                     if (monthOrYear.equals("Month")) {
-                        graph.getGridLabelRenderer().setLabelFormatter(new YearMonthFormatter());
+                        graph.getGridLabelRenderer().setLabelFormatter(new MonthFormatter());
                     } else {
                         graph.getGridLabelRenderer().setLabelFormatter(new YearFormatter());
                     }
                     graph.addSeries(series);
 
-                    graph.getViewport().setMinX(yearMonths.get(0) - 1);
-                    graph.getViewport().setMaxX(yearMonths.get(yearMonths.size() - 1) + 2);
+                    //Makes the graph look nice
+                    graph.getViewport().setMinX(dates.get(0) - 1);
+                    graph.getViewport().setMaxX(dates.get(dates.size() - 1) + 2);
+                    graph.getViewport().setMinY(0);
                 }
 
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.e("graph", "Database error occurred.");
             }
         });
 
@@ -280,4 +195,72 @@ public class GraphActivity extends Fragment {
         super.onStop();
     }
 
+    /**
+     * Calculates the number of months a specific month is from the beginning of the year 1970
+     * with Jan 1970 taking the value 1.
+     *
+     * @param month the month as an int (0-11)
+     * @param year the year as a 4 digit int
+     * @return the number of months since the beginning of 1970
+     */
+    private int calculateUnixTimeInMonths(int month, int year) {
+        int retval = month;
+        retval += (year - 1970) * 12;
+        return retval;
+    }
+
+    /**
+     * Takes in a unixMonth value and returns the month as an int 0-11
+     * @param unixMonth the unixMonth value to be converted
+     * @return the month as an int 0-11
+     */
+    private int unixMonthToMonth(int unixMonth) {
+        return unixMonth % 12;
+    }
+
+    /**
+     * Takes in a unixMonth value and returns the year as a 4 digit int
+     * @param unixMonth the unixMonth value to be converted
+     * @return the year as a 4 digit int
+     */
+    private int unixMonthToYear(int unixMonth) {
+        return 1970 + unixMonth / 12;
+    }
+
+    /**
+     * Formats axes for sorting rat sightings by month.
+     */
+    private class MonthFormatter extends DefaultLabelFormatter {
+        @Override
+        public String formatLabel(double value, boolean isValueX) {
+            String month;
+            String[] months = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+            "Sep", "Oct", "Nov", "Dec"};
+            String year;
+            if (isValueX) {
+                int unixMonth = (int) value;
+                month = months[unixMonthToMonth(unixMonth)];
+                year = Integer.toString(unixMonthToYear(unixMonth));
+                return month + " " + year;
+            } else {
+                return super.formatLabel(value, isValueX);
+            }
+        }
+    }
+
+    /**
+     * Formats axes for sorting rat sightings by year.
+     */
+    private class YearFormatter extends DefaultLabelFormatter {
+        @Override
+        public String formatLabel(double value, boolean isValueX) {
+            String year;
+            if (isValueX) {
+                year = Integer.toString((int)value);
+                return year;
+            } else {
+                return super.formatLabel(value, isValueX);
+            }
+        }
+    }
 }
